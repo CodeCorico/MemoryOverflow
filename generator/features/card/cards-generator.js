790a190490376375fc646ecdca5f1030868adfe2
groyer <guillaumeroyer.mail@gmail.com>
2014-09-18 20:39:13 +0200

    feat(generator): generate files to the website directory
    - Display progress bar on image generation complete
    
    Issue #2

--- generator/features/card/cards-generator.js
+++ generator/features/card/cards-generator.js
@@ -1,61 +1,48 @@
 (function() {
+  'use strict';
 
-  var fs = require('fs'),
-      Card = require('./card').Card;
-
-  function _listFiles(dir, done) {
-    var results = [];
-
-    fs.readdir(dir, function(err, list) {
-      if (err) {
-        return done(err);
-      }
-
-      var pending = list.length;
-      if (!pending) {
-        return done(null, results);
-      }
-
-      list.forEach(function(file) {
-        file = dir + '/' + file;
-
-        fs.stat(file, function(err, stat) {
-          if (stat && stat.isDirectory()) {
-            _listFiles(file, function(err, res) {
-              results = results.concat(res);
-              if (!--pending) {
-                done(null, results);
-              }
-            });
-          }
-          else {
-            results.push(file);
-            if (!--pending) {
-              done(null, results);
-            }
-          }
-        });
-      });
-    });
-  };
+  var Card = require('./card').Card,
+      FileUtils = require('../file/file.js').File,
+      ProgressBar = require('progress');
 
   function _endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
   }
 
   var CardsGenerator = function(templateFile, templateName) {
+    var bar = null;
+
+    function _tick() {
+      if (bar) {
+        bar.tick();
+      }
+    }
 
     this.generate = function() {
 
-      _listFiles('../cards', function(error, files) {
+      FileUtils.listFiles('../cards', function(error, files) {
+
+        var cards = [];
 
         if (!error) {
           files.forEach(function(file) {
             if (_endsWith(file, 'md') && !_endsWith(file, 'README.md')) {
-              new Card(file, templateFile, templateName).generate();
+              cards.push(new Card(file, templateFile, templateName));
             }
-
           });
+        }
+
+        var len = cards.length;
+
+        bar = new ProgressBar('Generating cards [:bar] :current / :total', {
+          complete: '=',
+          incomplete: ' ',
+          width: len * 4,
+          total: len
+        });
+
+        for (var i = 0; i < len; i++) {
+          cards[i].generate(_tick);
         }
 
       });
