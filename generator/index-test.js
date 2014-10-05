7849d17946f9a3253d377d42ca5b0ab726b04d27
groyer <guillaumeroyer.mail@gmail.com>
2014-10-05 18:03:53 +0200

    feat(generator): add generation simple test
    issue #2

--- generator/index-test.js
+++ generator/index-test.js
@@ -2,12 +2,22 @@
   'use strict';
 
   var assert = require('chai').assert,
-      fs = require('fs');
+      fs = require('fs'),
+      Generator = require('./index').Generator;
 
   describe('#valid', function() {
 
     it('should have a readme file', function() {
       assert(fs.existsSync('./README.md') === true, 'Project has a readme file');
+    });
+
+    it('should generate cards', function(done) {
+      new Generator(null).generate(function() {
+
+        assert(fs.existsSync('../website/data/cards') === true, 'Cards folder has been generated');
+
+        done();
+      });
     });
 
   });
