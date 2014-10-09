259f47e1958387ac6b69ced8bb07be3e557d0c10
Xavier Boubert <xavier@boubert.fr>
2014-10-10 00:00:31 +0200

    feat(the-machine): add The Machine agents tests

--- the-machine/features/the-machine/the-machine-test.js
+++ the-machine/features/the-machine/the-machine-test.js
@@ -3,26 +3,27 @@
 
   var assert = require('chai').assert,
       path = require('path'),
-      fs = require('fs');
+      fs = require('fs'),
+      theMachine = null,
+      theMachinePath = path.resolve(path.join(__dirname, 'the-machine.js')),
+      agent = null,
+      agentPath = path.resolve(path.join(__dirname, 'the-machine-agent.js')),
+      log = null,
+      lastLog = '';
+
+  function _stopConsoleLog() {
+    log = console.log;
+
+    console.log = function(string) {
+      lastLog = string;
+    };
+  }
+
+  function _startConsoleLog() {
+    console.log = log;
+  }
 
   describe('The Machine', function() {
-
-    var theMachine = null,
-        theMachinePath = path.resolve(path.join(__dirname, 'the-machine.js')),
-        log = null,
-        lastLog = '';
-
-    function _stopConsoleLog() {
-      log = console.log;
-
-      console.log = function(string) {
-        lastLog = string;
-      };
-    }
-
-    function _startConsoleLog() {
-      console.log = log;
-    }
 
     it('should start', function() {
       assert(fs.existsSync(theMachinePath), 'the-machine.js file exists');
@@ -57,6 +58,50 @@
       _startConsoleLog();
 
       assert(lastLog.indexOf('The Machine:') > -1 && lastLog.indexOf('Hello.') > -1, 'The Machine knows how to answer.');
+    });
+
+  });
+
+  describe('The Special Agents', function() {
+
+    it('should start', function() {
+      assert(fs.existsSync(agentPath), 'the-machine-agent.js file exists');
+
+      _stopConsoleLog();
+      var TheMachineAgent = require(agentPath),
+          inError = false;
+
+      try {
+        agent = new (function() {
+          TheMachineAgent.call(this, theMachine);
+
+          this.name('of tests');
+        })();
+      }
+      catch(error) {
+        inError = true;
+      }
+
+      _startConsoleLog();
+      assert(!inError, 'Start Agent of tests');
+    });
+
+    it('should communicate with The Machine', function() {
+      var sentence = 'Boss are you there?';
+
+      _stopConsoleLog();
+      agent.says(sentence);
+
+      _startConsoleLog();
+      assert(lastLog.indexOf('Agent of tests:') > -1 && lastLog.indexOf(sentence) > -1, 'Agent knows how to communicate.');
+
+      _stopConsoleLog();
+      agent.says(sentence, {
+        needHello: true
+      });
+      _startConsoleLog();
+
+      assert(lastLog.indexOf('The Machine:') > -1 && lastLog.indexOf('Hello.') > -1, 'Agent knows how to communicate with The Machine.');
     });
 
   });
