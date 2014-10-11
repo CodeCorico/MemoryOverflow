b1e601b49b36bf625f8df9e8e84a006d9220980a
groyer <guillaumeroyer.mail@gmail.com>
2014-10-11 14:27:46 +0200

    feat(card): add unit tests

---
+++ the-machine/features/card/card-test.js
@@ -0,0 +1,82 @@
+(function() {
+  'use strict';
+
+  var expect = require('chai').expect,
+      path = require('path'),
+      fs = require('fs'),
+      rootDirectory = path.join(__dirname, '..', '..', '..'),
+      cardPath = path.resolve(path.join(__dirname, 'card.js'));
+
+  describe('Card', function() {
+
+    function _testCardLoad(cardDirectory, cardName, template, lang, done) {
+      expect(fs.existsSync(cardPath)).to.be.true;
+
+      var Card = require(cardPath),
+          cardMarkdownFile = path.join(cardDirectory, cardName + '.md'),
+          inError = false;
+
+      expect(fs.existsSync(rootDirectory)).to.be.true;
+      expect(fs.existsSync(cardDirectory)).to.be.true;
+      expect(fs.existsSync(cardMarkdownFile)).to.be.true;
+
+      var card = new Card(cardMarkdownFile, template, cardName);
+      expect(card).not.to.be.null;
+
+      card.languageFiles(lang, path.join(cardDirectory, cardName + '.' + lang + '.po'));
+
+      card.load(function(error, loadedCards) {
+        expect(error).to.be.null;
+        expect(loadedCards.length).not.to.be.empty;
+
+        loadedCards.forEach(function(card) {
+          expect(card.name).to.equal(cardName);
+          expect(card.template).to.equal(template);
+          expect(card.lang).to.equal(lang);
+          expect(card.code).not.to.be.empty;
+          expect(card.html).not.to.be.empty;
+        });
+
+        done();
+      });
+    }
+
+    it('should load the card \'foo\' in french', function(done) {
+
+      var cardName = 'foo',
+          cardDirectory = path.join(rootDirectory, 'cards', 'fundamentals', 'foo'),
+          template = 'fototse',
+          lang = 'fr_FR',
+          inError = false;
+
+      try {
+        _testCardLoad(cardDirectory, cardName, template, lang, done);
+      }
+      catch(error) {
+        inError = true;
+      }
+
+      expect(inError).to.be.false;
+    });
+
+    it('should load the card \'foo\' in english', function(done) {
+
+      var cardName = 'foo',
+          cardDirectory = path.join(rootDirectory, 'cards', 'fundamentals', 'foo'),
+          template = 'fototse',
+          lang = 'en_EN',
+          inError = false;
+
+      try {
+        _testCardLoad(cardDirectory, cardName, template, lang, done);
+      }
+      catch(error) {
+        inError = true;
+      }
+
+      expect(inError).to.be.false;
+    });
+
+  });
+
+})();
