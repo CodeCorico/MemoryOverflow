ffc53e1f4e5f7e0448a91cc874bce91019f918cb
Xavier Boubert <xavier@boubert.fr>
2014-10-11 13:24:53 +0200

    feat(parser markdown): add unit tests

---
+++ the-machine/features/parser-markdown/parser-markdown-test.js
@@ -0,0 +1,37 @@
+(function() {
+  'use strict';
+
+  var expect = require('chai').expect,
+      path = require('path');
+
+  describe('Parser markdown', function() {
+
+    it('should parse', function() {
+
+      var parserMarkdown = require(path.join(__dirname, 'parser-markdown.js')),
+          testArray = [
+            '# Title 1',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at elit eu lectus tristique malesuada. Aliquam ut felis eget urna ullamcorper efficitur. Praesent tempus euismod est sed elementum. Aenean ut malesuada est. Nam ullamcorper, odio at sodales varius, lectus ipsum euismod odio, sed vehicula ligula elit eu turpis. Nulla in enim nec urna pretium mollis. In condimentum ut ante a accumsan. Donec fringilla interdum est vel blandit. Donec at malesuada sapien. Sed ut sollicitudin metus. Duis sed dapibus metus, eu convallis velit. In velit turpis, ullamcorper sed erat vitae, interdum aliquet arcu.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at elit eu lectus tristique malesuada. Aliquam ut felis eget urna ullamcorper efficitur. Praesent tempus euismod est sed elementum. Aenean ut malesuada est. Nam ullamcorper, odio at sodales varius, lectus ipsum euismod odio, sed vehicula ligula elit eu turpis. Nulla in enim nec urna pretium mollis. In condimentum ut ante a accumsan. Donec fringilla interdum est vel blandit. Donec at malesuada sapien. Sed ut sollicitudin metus. Duis sed dapibus metus, eu convallis velit. In velit turpis, ullamcorper sed erat vitae, interdum aliquet arcu.',
+            '# Title 2 # fake text',
+            'Another text with # for tests',
+            '# Title 3',
+            '# Title 4',
+            '',
+            'The Machine is everywhere.'
+          ],
+          testText = testArray.join('\n'),
+          expected = {
+            'Title 1': testArray[1] + '\n' + testArray[2],
+            'Title 2 # fake text': testArray[4],
+            'Title 3': '',
+            'Title 4': testArray[8]
+          };
+
+      expect(parserMarkdown(testText)).to.deep.equal(expected);
+
+    });
+
+  });
+
+})();
