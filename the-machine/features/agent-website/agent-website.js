8c69ef212d2e92c4c476cbfe5e9d6786e5b9be2d
Xavier Boubert <xavier@boubert.fr>
2014-10-20 22:41:11 +0200

    feat(agent-website): create the Agent of website
    He watch all website ejs files
    He regenerate the HTML file of the ejs file if it changed
    In the case of its a features/*.ejs file changing, he regenereate all of the index ejs files
    
    Issue #20

---
+++ the-machine/features/agent-website/agent-website.js
@@ -0,0 +1,158 @@
+(function() {
+  'use strict';
+
+  var TheMachineAgent = require('../the-machine/the-machine-agent.js'),
+      fs = require('fs-extra'),
+      path = require('path'),
+      ejs = require('ejs'),
+      glob = require('glob'),
+
+      PATHS = {
+        WEBSITE_EJS: '../website/**/*.ejs'
+      };
+
+  var AgentWebsite = function(theMachine) {
+    TheMachineAgent.call(this, theMachine);
+
+    var _this = this;
+
+    function _init() {
+      _this.name('of Website');
+
+      _this.says('Website acquired, I\'m watching this files.');
+
+      _this
+        .watch([PATHS.WEBSITE_EJS], function(args) {
+          var filePath = args.path.split(path.sep),
+              file = filePath.pop(),
+              folder = filePath.join(path.sep);
+
+          _work(args.type, file, folder);
+        });
+    }
+
+    function _work(type, ejsFile, folder) {
+      var htmlFile = ejsFile.replace('.ejs', '.html'),
+          htmlDestination = path.join(folder, htmlFile),
+          ejsSource = path.join(folder, ejsFile);
+
+      _this.newDiscussion();
+
+      if(folder.indexOf(path.sep + 'features' + path.sep) > -1) {
+        _this.says('Boss, a website feature has been ' + type + '. I have to regenerate all the HTML files.');
+
+
+        var ejsfiles = glob.sync(PATHS.WEBSITE_EJS).map(function(file) {
+
+          var source = path.resolve(file),
+              destination = source.replace('.ejs', '.html');
+
+          return {
+            source: source,
+            destination: destination
+          };
+        }).filter(function(file) {
+          return file.source.indexOf(path.sep + 'features' + path.sep) === -1;
+        });
+
+        _generate(ejsfiles, function(success, error) {
+          if(success) {
+            _this.says('All HTML final files generated. Everything is ok.', {
+              needGratitude: true
+            });
+          }
+          else{
+            _this.saysError(error, {
+              needSlaps: true
+            });
+          }
+        });
+      }
+      else if(type == 'deleted') {
+        _this.says('Boss, the website "' + ejsFile + '" file has been deleted. I have to delete the HTML file.');
+
+        _delete(htmlDestination, function(success, error) {
+          if(success) {
+            _this.says('"' + htmlFile + '" file deleted. Everything is ok.', {
+              needGratitude: true
+            });
+          }
+          else{
+            _this.saysError(error, {
+              needSlaps: true
+            });
+          }
+        });
+      }
+      else {
+        _this.says('Boss, the website "' + ejsFile + '" file has been updated. I have to regenerate it.');
+
+        _generate([{
+          source: ejsSource,
+          destination: htmlDestination
+        }], function(success, error) {
+          if(success) {
+            _this.says('"' + htmlFile + '" file generated. Everything is ok.', {
+              needGratitude: true
+            });
+          }
+          else{
+            _this.saysError(error, {
+              needSlaps: true
+            });
+          }
+        });
+      }
+    }
+
+    function _delete(htmlDestination, callback) {
+      callback = callback || function() {};
+
+      fs.removeSync(htmlDestination);
+
+      callback(true);
+    }
+
+    function _generate(ejsfiles, callback) {
+      callback = callback || function() {};
+
+      var error = null;
+
+      for(var i = 0, len = ejsfiles.length; i < len; i++) {
+        var source = ejsfiles[i].source,
+            destination = ejsfiles[i].destination;
+
+        if(!fs.existsSync(source)) {
+          error = 'Mmm something wired appends, "' + source + '" didn\'t exists.';
+          break;
+        }
+
+        var ejsString = fs.readFileSync(source, 'utf8');
+        if(!ejsString) {
+          error = 'Mmm I can\'t read "' + source + '".';
+          break;
+        }
+
+        var html = ejs.render(ejsString, {
+          filename: source,
+          css: [],
+          js: []
+        });
+
+        if(!html) {
+          error = 'Sorry but "' + source  + '" contains EJS formatting errors. Please fix it!';
+          break;
+        }
+
+        fs.outputFileSync(destination, html);
+      }
+
+      return callback(!error, error);
+    }
+
+    _init();
+  };
+
+  module.exports = AgentWebsite;
+
+ })();
