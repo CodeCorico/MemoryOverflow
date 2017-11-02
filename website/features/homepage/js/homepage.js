5a1ba269ba97e763a385c703b5514f2d9f11e952
Xavier Boubert <xavier@boubert.fr>
2017-11-02 16:02:11 +0100

    feat(website): redirect the user in translated websites
    Issue #35

--- website/features/homepage/js/homepage.js
+++ website/features/homepage/js/homepage.js
@@ -5,14 +5,67 @@ $(function() {
     return;
   }
 
-  var _panelsVisibilityMargin = 0,
-      _panels = [{
-        selector: '.panel-game'
-      }, {
-        selector: '.panel-cards'
-      }, {
-        selector: '.panel-contribute'
-      }];
+  // 0 is the default
+  var LANGS = ['en', 'fr'];
+  var langKey;
+  var validLang = LANGS[0];
+  var langForced = location.href.indexOf('?l=f') > -1;
+
+  if (langForced) {
+    langForced = false;
+
+    var actualLang = location.pathname == '/' ? LANGS[0] : location.pathname.split('/')[1];
+
+    for (langKey in LANGS) {
+      if (actualLang.indexOf(LANGS[langKey]) > -1) {
+        Cookies.set('lang', actualLang);
+
+        langForced = true;
+
+        break;
+      }
+    }
+  }
+
+  if (!langForced) {
+    var language = Cookies.get('lang') || window.navigator.language || window.navigator.userLanguage;
+    language = language.toLowerCase();
+
+    for (langKey in LANGS) {
+      if (language.indexOf(LANGS[langKey]) > -1) {
+        validLang = LANGS[langKey];
+
+        if (!Cookies.get('lang')) {
+          Cookies.set('lang', validLang);
+        }
+
+        break;
+      }
+    }
+
+    if (validLang != LANGS[0] && location.pathname == '/') {
+      location.href = '/' + validLang;
+    }
+    else if (validLang == LANGS[0] && location.pathname != '/') {
+      location.href = '/';
+    }
+    else if (location.pathname != '/') {
+      var actualLang = location.pathname.split('/')[1];
+
+      if (actualLang != validLang) {
+        location.href = '/' + validLang;
+      }
+    }
+  }
+
+  var _panelsVisibilityMargin = 0;
+  var _panels = [{
+    selector: '.panel-game'
+  }, {
+    selector: '.panel-cards'
+  }, {
+    selector: '.panel-contribute'
+  }];
 
   function _init() {
     $.each(_panels, function(index, panel) {
@@ -83,5 +136,4 @@ $(function() {
   });
 
   _init();
-
 });
