2860162ff5c45e9a7bc629cae4dc9d468f9fde56
Xavier Boubert <xavier@boubert.fr>
2017-11-02 14:51:26 +0100

    refactor(the machine): move to ES6

--- the-machine/features/the-machine/the-machine.js
+++ the-machine/features/the-machine/the-machine.js
@@ -1,147 +1,141 @@
-(function() {
-  'use strict';
+const gulp = require('gulp');
+const clc = require('cli-color');
+const glob = require('glob');
+const extend = require('extend');
 
-  var gulp = require('gulp'),
-      clc = require('cli-color'),
-      glob = require('glob'),
-      extend = require('extend');
+module.exports = function TheMachine(oneShot) {
+  oneShot = oneShot || false;
 
-  var TheMachine = function(oneShot) {
-    oneShot = oneShot || false;
+  const _agents = [];
 
-    var _this = this,
-        _agents = [];
+  const init = () => {
+    if (clc.windowSize.width) {
+      const bannerWidth = 52;
+      const fillCharasNb = Math.floor((clc.windowSize.width - bannerWidth) / 2);
+      const margin = fillCharasNb > 0 ? new Array(Math.floor((clc.windowSize.width - bannerWidth) / 2)).join(' ') : '';
+      const fillBar = margin.replace(/ /g, '░');
 
-    function _init() {
-      if(clc.windowSize.width) {
-        var bannerWidth = 52,
-            fillCharasNb = Math.floor((clc.windowSize.width - bannerWidth) / 2),
-            margin = fillCharasNb > 0 ? new Array(Math.floor((clc.windowSize.width - bannerWidth) / 2)).join(' ') : '',
-            fillBar = margin.replace(/ /g, '░');
+      const banner = [
+        clc.red(fillBar + '░░░░░░░░░░░░░░░░                    ░░░░░░░░░░░░░░░░' + fillBar) + '\n',
+        clc.red(          '             ░░                      ░░             ') + '\n',
+        clc.red(          '           ░░                          ░░           ') + '\n',
+        clc.red(          '         ░░                              ░░         ') + '\n',
+        clc.red(          '         ░░   ' +                 ' ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ' +           '   ░░         ') + '\n',
+        clc.red(          '         ░░   ') + clc.redBright('████████████████████████') + clc.red('   ░░         ') + '\n',
+        clc.red(          '         ░░   ' +                 ' ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ' +           '   ░░         ') + '\n',
+        clc.red(          '         ░░                              ░░         ') + '\n',
+        clc.red(          '           ░░                          ░░           ') + '\n',
+        clc.red(          '             ░░                      ░░             ') + '\n',
+        clc.red(fillBar + '░░░░░░░░░░░░░░░░                    ░░░░░░░░░░░░░░░░' + fillBar) + '\n\n',
+        clc.red(          '           « The Machine sees everything »') + '\n\n'
+      ];
 
-        var banner = [
-          clc.red(fillBar + '░░░░░░░░░░░░░░░░                    ░░░░░░░░░░░░░░░░' + fillBar) + '\n',
-          clc.red(          '             ░░                      ░░             ') + '\n',
-          clc.red(          '           ░░                          ░░           ') + '\n',
-          clc.red(          '         ░░                              ░░         ') + '\n',
-          clc.red(          '         ░░   ' +                 ' ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ' +           '   ░░         ') + '\n',
-          clc.red(          '         ░░   ') + clc.redBright('████████████████████████') + clc.red('   ░░         ') + '\n',
-          clc.red(          '         ░░   ' +                 ' ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ' +           '   ░░         ') + '\n',
-          clc.red(          '         ░░                              ░░         ') + '\n',
-          clc.red(          '           ░░                          ░░           ') + '\n',
-          clc.red(          '             ░░                      ░░             ') + '\n',
-          clc.red(fillBar + '░░░░░░░░░░░░░░░░                    ░░░░░░░░░░░░░░░░' + fillBar) + '\n\n',
-          clc.red(          '           « The Machine sees everything »') + '\n\n'
-        ];
-
-        for(var i = 0, len = banner.length; i < len; i++) {
-          if(i !== 0 && i != 10) {
-            banner[i] = margin + banner[i];
-          }
+      for(let i = 0, len = banner.length; i < len; i++) {
+        if(i !== 0 && i != 10) {
+          banner[i] = margin + banner[i];
         }
-
-        console.log([clc.reset].concat(banner).join(''));
       }
 
-      if(oneShot) {
-        _this.says('Special agents, go generate your targets.');
-      }
-      else {
-        _this.says('Special agents, go watching your targets.');
-      }
+      console.log([clc.reset].concat(banner).join(''));
+    }
 
-      var files = glob.sync('./features/**/agent-*.js');
+    if(oneShot) {
+      this.says('Special agents, go generate your targets.');
+    }
+    else {
+      this.says('Special agents, go watching your targets.');
+    }
 
-      if(files || files.length) {
-        files.forEach(function(file) {
-          if(file.indexOf('-test') > -1) {
-            return;
-          }
+    let files = glob.sync('./features/**/agent-*.js');
 
-          var agent = require(file.replace('./features', '../'));
-          _agents.push(new agent(_this));
-        });
-
-        if(oneShot) {
+    if(files || files.length) {
+      files.forEach((file) => {
+        if(file.indexOf('-test') > -1) {
           return;
         }
 
-        gulp.task('watch', function() {
-          _agents.forEach(function(agent) {
-            agent.watchers().forEach(function(watcher) {
-              gulp.watch(watcher.files, watcher.func);
-            });
-          });
-        });
-      }
+        let agent = require(file.replace('./features', '../'));
 
-      gulp.task('default', ['watch']);
-    }
-
-    this.isOneShot = function() {
-      return oneShot;
-    };
-
-    this.says = function(sentence) {
-      process.stdout.write(clc.red('The Machine: '));
+        _agents.push(new agent(this));
+      });
 
       if(oneShot) {
-        process.stdout.write(clc.redBright(sentence));
-      }
-      else {
-        for(var i = 0, len = sentence.length; i < len; i++) {
-          process.stdout.write(clc.redBright(sentence[i]));
-          var wait = 10,
-              stop = 1 * new Date();
-          while(1 * new Date() < stop + wait) { }
-        }
-      }
-      process.stdout.write('\n');
-
-      return _this;
-    };
-
-    this.answers = function(options) {
-      options = extend(true, {
-        needHello: false,
-        needGratitude: false,
-        needSlaps: false
-      }, options || {});
-
-      var sentences = [];
-
-      if(options.needHello) {
-        sentences = ['Hello.'];
-      }
-      else if(options.needGratitude) {
-        sentences = [
-          'Good work son.',
-          'Proud of you.',
-          'Got it.',
-          'Cool cool cool.',
-          'Fantastic.'
-        ];
-      }
-      else if(options.needSlaps) {
-        sentences = [
-          'Looks like it\'s a problem',
-          'Ho god!',
-          'Damn it!',
-          '$%#!'
-        ];
+        return;
       }
 
-      if(sentences.length > 0) {
-        _this.says(sentences[Math.floor(Math.random() * sentences.length)]);
-      }
+      gulp.task('watch', () => {
+        _agents.forEach((agent) => {
+          agent.watchers().forEach((watcher) => {
+            gulp.watch(watcher.files, watcher.func);
+          });
+        });
+      });
+    }
 
-      return _this;
-    };
-
-    _init();
-
+    gulp.task('default', ['watch']);
   };
 
-  module.exports = TheMachine;
+  this.isOneShot = () => {
+    return oneShot;
+  };
 
- })();
+  this.says = (sentence) => {
+    process.stdout.write(clc.red('The Machine: '));
+
+    if(oneShot) {
+      process.stdout.write(clc.redBright(sentence));
+    }
+    else {
+      for (let i = 0, len = sentence.length; i < len; i++) {
+        process.stdout.write(clc.redBright(sentence[i]));
+
+        let wait = 10;
+        let stop = 1 * new Date();
+
+        while(1 * new Date() < stop + wait) { }
+      }
+    }
+    process.stdout.write('\n');
+
+    return this;
+  };
+
+  this.answers = (options) => {
+    options = extend(true, {
+      needHello: false,
+      needGratitude: false,
+      needSlaps: false
+    }, options || {});
+
+    let sentences = [];
+
+    if(options.needHello) {
+      sentences = ['Hello.'];
+    }
+    else if(options.needGratitude) {
+      sentences = [
+        'Good work son.',
+        'Proud of you.',
+        'Got it.',
+        'Cool cool cool.',
+        'Fantastic.'
+      ];
+    }
+    else if(options.needSlaps) {
+      sentences = [
+        'Looks like it\'s a problem',
+        'Ho god!',
+        'Damn it!',
+        '$%#!'
+      ];
+    }
+
+    if(sentences.length > 0) {
+      this.says(sentences[Math.floor(Math.random() * sentences.length)]);
+    }
+
+    return this;
+  };
+
+  init();
+};
